17c7871505a7afdad760ee213c277c8f42c13d5f
Henry Sloan <bakmanthetitan329@gmail.com>
2017-06-27 14:00:30 -0400

    Add modular posts and better sidebar

--- App.jsx
+++ App.jsx
@@ -1,7 +1,57 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
 
-function clickDelete(e) {
-   document.getElementById('test-post').remove();
+class Post extends React.Component {
+   constructor(props){
+      super(props);
+      this.state = {render: true};
+      this.clickDelete = this.clickDelete.bind(this);
+   }
+
+   clickDelete() {
+      this.setState({render: false});
+   }
+
+   actions() {
+      if (this.props.name == "John Doe") {
+         return(
+         <div id="actions">
+            <a href="#">Like</a>
+            <a href="#">Comment</a>
+            <a href="javascript:void(0);" onClick={this.clickDelete}>Delete</a>
+         </div>);
+      }
+      else {
+         return(
+         <div id="actions">
+            <a href="#">Like</a>
+            <a href="#">Comment</a>
+            <a href="#">Share</a>
+         </div>);
+      }
+   }
+
+   render() {
+      if (!this.state.render) {
+         return null;
+      }
+
+      return(
+         <div id="test-post">
+            <div id="test-content">
+               <div id="test-info">
+                  <img id="test-pic" src={this.props.img} />
+                  <div id="test-text">
+                     <a href="#" id="test-name">{this.props.name}</a>
+                     <p id="test-time">1 hr</p>
+                  </div>
+               </div>
+               <p>{this.props.children}</p>
+               <hr />
+               {this.actions()}
+            </div>
+         </div>);
+   }
 }
 
 class App extends React.Component {
@@ -22,32 +72,17 @@ class App extends React.Component {
             <div id="page-content">
                <ul id="left-navagation">
                   <li>
+                     <img src="./assets/profile_img.jpg" />
                      <a href="#">John Doe</a>
                   </li>
                   <li>
+                     <img src="./assets/news_feed.jpg" />    
                      <a href="#">News Feed</a>
                   </li>
                </ul>
                <div id="post-area">
-                  <div id="test-post">
-                     <div id="test-content">
-                        <div id="test-info">
-                           <img id="test-pic" src="./assets/profile_img.jpg" />
-                           <div id="test-text">
-                              <a href="#" id="test-name">John Doe</a>
-                              <p id="test-time">1 hr</p>
-                           </div>
-                        </div>
-                        <h3>Test post</h3>
-                        <hr />
-                        <div id="actions">
-                           <a href="#">Like</a>
-                           <a href="#">Comment</a>
-                           <a href="#">Share</a>
-                           <a  href="javascript:void(0);" onClick={clickDelete}>Delete</a>
-                        </div>
-                     </div>
-                  </div>
+                  <Post name="John Doe" img="./assets/profile_img.jpg">Hi, I'm John.</Post>
+                  <Post name="Jack Roe" img="./assets/profile_img.jpg">There is a party at my house tommorow.</Post>
                </div>
             </div>
          </div>

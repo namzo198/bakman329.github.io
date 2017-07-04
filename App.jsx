b8262a560a41c3af0bfd3ebbd20a725162da7c24
Henry Sloan <bakmanthetitan329@gmail.com>
2017-07-04 15:43:04 -0400

    Add abstract buttons and postlist, and add locally stored persistant posts.

--- App.jsx
+++ App.jsx
@@ -2,10 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {CreateEvent} from './controller/databaseFunctions.js';
 
+class Button extends React.Component {
+   constructor(props) {
+      super(props);
+      this.onClick = this.onClick.bind(this);
+   }
+
+   onClick() {
+      var state = this.props.onClick();
+      var event = {action : state.action,
+                   details : state.context,
+                   object : state.name};
+      CreateEvent(event);
+   }
+
+   render() {
+      return (<a href={this.props.href} onClick={this.onClick}>{this.props.children}</a>);
+   }
+}
+
 class Post extends React.Component {
-    
-    
-    
    constructor(props){
       super(props);
       this.state = {
@@ -15,30 +31,30 @@ class Post extends React.Component {
           action:""};
       this.clickDelete = this.clickDelete.bind(this);
    }
-    
-    
-    
 
    clickDelete(){
-       
-     this.setState({
+     var event = {
          render:false,
          action:'Deleted',
-         name:'John Doe\'s Post' //This needs to always be updated based on Post Id
-     });
-       
-       
+         context: this.state.context,
+         name: this.props.name + '\'s Post'
+     };
+     this.setState(event);
+     var posts = localStorage.getItem("posts").split('');
+     posts[Number(this.props.postid)*2] = 0;
+     localStorage.setItem("posts", posts.join(''));
+
+     return event;
    }
 
    actions() {
        
       if (this.props.name == "John Doe") {
-          
          return(
          <div id="actions">
             <a href="#">Like</a>
             <a href="#">Comment</a>
-            <a href="javascript:void(0);" onClick={this.clickDelete}>Delete</a>
+            <Button href="javascript:void(0);" onClick={this.clickDelete}>Delete</Button>
          </div>);
       }
       else {
@@ -52,28 +68,19 @@ class Post extends React.Component {
    }
 
    render() {
-      
-      if (!this.state.render) {  
-          
-          //Create event variable
-      var event = {
-          action : this.state.action,
-          details : this.state.context, object  : this.state.name
-         };
-          //Call CreateEvent function
-         CreateEvent(event);
-          
+      // Multiply index by two to skip commas in string
+      if (localStorage.getItem('posts')[this.props.postid*2] == 0) {
          return null;
       }
 
       return(
-         <div id="test-post">
-            <div id="test-content">
-               <div id="test-info">
-                  <img id="test-pic" src={this.props.img} />
-                  <div id="test-text">
-                     <a href="#" id="test-name">{this.props.name}</a>
-                     <p id="test-time">1 hr</p>
+         <div id="post">
+            <div id="post-content">
+               <div id="post-info">
+                  <img id="post-pic" src={this.props.img} />
+                  <div id="post-text">
+                     <a href="#" id="post-name">{this.props.name}</a>
+                     <p id="post-time">1 hr</p>
                   </div>
                </div>
                <p>{this.props.children}</p>
@@ -84,8 +91,25 @@ class Post extends React.Component {
    }
 }
 
+class PostArea extends React.Component {
+   constructor(props) {
+      super(props);
+      this.count = React.Children.count(this.props.children)
+      // If null, init to 1's
+      if (localStorage.getItem("posts") == null) {
+         localStorage.setItem("posts", (new Array(this.count).fill(1, 0, this.count)));
+      }
+   }
+
+   render() {
+      return (
+         <div id="post-area">
+            {this.props.children}
+         </div>);
+   }
+}
+
 class App extends React.Component {
-    
    render() {
       return (
          <div>
@@ -110,11 +134,14 @@ class App extends React.Component {
                      <img src="./assets/news_feed.jpg" />    
                      <a href="#">News Feed</a>
                   </li>
+                  <li>
+                     <a href="javascript:void(0)" onClick={() => { localStorage.setItem("posts", null); location.reload(); }}>Reset Posts(DEBUG)</a>
+                  </li>
                </ul>
-               <div id="post-area">
-                  <Post name="John Doe" img="./assets/profile_img.jpg">Hi, I'm John.</Post>
-                  <Post name="Jack Roe" img="./assets/profile_img.jpg">There is a party at my house tommorow.</Post>
-               </div>
+               <PostArea>
+                  <Post name="John Doe" img="./assets/profile_img.jpg" postid='0'>Hi, I'm John.</Post>
+                  <Post name="Jack Roe" img="./assets/profile_img.jpg" postid='1'>There is a party at my house tommorow.</Post>
+               </PostArea>
             </div>
          </div>
       );

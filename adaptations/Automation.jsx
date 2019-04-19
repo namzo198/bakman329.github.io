0de3fa0e8dba3c9c9ff535d2cb829c8bee4df510
saiharshanani <34852063+saiharshanani@users.noreply.github.com>
2019-04-19 13:38:43 -0400

    Adaptations Update
    Updates for
    - Highlight inline styling
    - Suggestion box interface
    - Auto Box interface

--- adaptations/Automation.jsx
+++ adaptations/Automation.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import Button from '../components/Button.jsx'
-    
+
 class Automation extends React.Component{
   render() {
    var okButton_option;
-   
+
    if (this.props.onOkClick !== undefined) {
       okButton_option = <Button id='undo' href='javascript:void(0)' onClick={this.props.onOkClick}>{this.props.okButton}</Button>;
     }
-     
+
+    var divStyle = {paddingLeft: '40px'}
+
     return(
-        <div>
-            <label>{this.props.label}</label><Button id='undo' href='javascript:void(0)' onClick={this.props.onUndoClick}>{this.props.undoButton}</Button>  
+        <div id = "automation-block"> <img src={'/assets/flag.png'} />
+
+            <label style={divStyle}>{this.props.label}</label><Button id='undo' href='javascript:void(0)' onClick={this.props.onUndoClick}>{this.props.undoButton}</Button>
              {okButton_option}
-             
+
         </div>
     );
   }
